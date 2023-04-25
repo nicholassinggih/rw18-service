be9676a0e3af9f60934a0dce93cb7d0d83d3a911
nicholasirsyad <nicholas.irsyad@akarinti.tech>
2023-04-25 21:44:28 +0700

    refactor(conn-pool):rename file and fix model maps
    -data-service was renamed to connection-pool
    -fix incorrect model mappings

--- service/connection-pool.mjs
+++ service/connection-pool.mjs
@@ -30,30 +30,6 @@ class ConnectionPool {
           }
     }
 
-    initializeSoundex = async function() {
-        
-    }
 }
-/* 
-const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
-}); */
 
 export default ConnectionPool;
