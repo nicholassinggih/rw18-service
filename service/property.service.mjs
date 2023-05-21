fa101bdefcad952666ce9d76a37e12cc67f4de34
nicholassinggih <nicholas.irsyad@akarinti.tech>
2023-05-21 22:19:55 +0700

    feat(property-svc):search by soundex
    -add criteria using soundex and order by relevance

--- service/property.service.mjs
+++ service/property.service.mjs
@@ -1,22 +1,31 @@
+import { Sequelize } from 'sequelize';
 import * as Models from '../models/definitions.mjs';
+import svc from '../util.mjs';
 
 class PropertyService {
   async search(keyword) {
     var res = null;
+    const keywordSoundex = svc.soundexText(keyword);
     try {
       res = await Models.Property.findAll({
-        // where: {
-        //   blok: 'A'
-        // },
+        attributes: {
+          include: [
+            [Sequelize.literal('MATCH (Pemilik.nama_soundex) AGAINST(?)'), 'relevance']
+          ]
+        },
+        replacements: [`*${keywordSoundex}*`, `*${keywordSoundex}*`],
         include: [
           {
             model: Models.Pemilik, 
-            where: {
-              nama: keyword
-            }
+            
           }
+        ],
+        where: Sequelize.literal('MATCH (Pemilik.nama_soundex) AGAINST(?)'),
+        order: [
+          ['relevance', 'DESC']
         ]
-      })
+        
+      }) 
     } catch(err) {
       console.log(err);
     }
@@ -39,27 +48,7 @@ class PropertyService {
       }
       return res; */
     }
-  /* 
-    async getPropertyById(id) {
-      return await Models.Property.findByPk(id);
-    }
   
-    async updateProperty(id, data) {
-      const prop = await Models.Property.findByPk(id);
-      if (prop) {
-        return await Models.Property.update(data);
-      }
-      return null;
-    }
-  
-    async deleteProperty(id) {
-      const prop = await Models.Property.findByPk(id);
-      if (prop) {
-        await Models.Property.destroy();
-        return true;
-      }
-      return false;
-    } */
   }
   
   export default PropertyService;
