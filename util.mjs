7cb04ed489439a5851e62952b0918d44e781432d
nicholassinggih <nicholas.irsyad@akarinti.tech>
2023-05-30 05:07:12 +0700

    feat(util):add logic & a function
    -encodeText filters empty results
    -isEmptyString function returns true when passed empty string

--- util.mjs
+++ util.mjs
@@ -9,7 +9,12 @@ svc.breakIntoWords = (str) => {
 svc.encodeText = (text) => {
     return svc.breakIntoWords(text).map(element => doubleMetaphone(element)[1])
         .map(w => w === "A"? "AA" : w)
+        .filter(x => !svc.isEmptyString(x))
         .join(" ");
+}
+
+svc.isEmptyString = (str) => {
+    return str == null || str.trim() == '';
 }
 
 export default svc;
