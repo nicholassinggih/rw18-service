db7f2d857399b704f00111194770773951da32b4
nicholassinggih <nicholas.irsyad@akarinti.tech>
2023-07-29 22:34:26 +0700

    feat(account-svc):add search by keyword
    -account controller accepts search lists of accounts by keywords
    -the keywords are matched through the property details

--- service/account.service.mjs
+++ service/account.service.mjs
@@ -1,7 +1,14 @@
 import * as Models from '../models/definitions.mjs';
 import PropertyService from './property.service.mjs';
+
 class AccountService {
   async getAllActive() { 
+    return this.search({
+      active: true
+    })
+  }
+
+  async search(criteria) {
     var res = [];
 
     let includeAttributes = [
@@ -18,14 +25,43 @@ class AccountService {
             model: Models.Property, 
           },
         ],
-        where: {
-          active: true
-        },
+        where: criteria,
       }) 
     } catch(err) {
       console.log(err);
     }
     return res;
+  }
+
+  async searchByKeyword(keyword, offset, limit) {
+    const ps = new PropertyService();
+    var res = [];
+    try {
+      const where = ps.createWhereCriteria(keyword);
+      console.log('values for where : ' + where.replacements);
+      res = await Models.Account.findAndCountAll({
+        include: [
+          {
+            model: Models.Property,
+            attributes: { include: [ PropertyService.selectCurrentFee ] },
+            ...PropertyService.includeModels,
+          }
+        ],
+        ...ps.createWhereCriteria(keyword, 'Property->')
+        
+      }) 
+    } catch(err) {
+      console.log(err);
+    }
+    return res;
+  } 
+
+  async getAccountForProp(property) {
+    return this.search({
+      propertyId: property.id,
+      active: true,
+      pemilikId: property.pemilikId
+    })
   }
   
 }
