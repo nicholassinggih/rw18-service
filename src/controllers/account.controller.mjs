00ae3693ebe9b51d1e73b48f50bd3d10978d023a
nicholassinggih <nicholas.irsyad@akarinti.tech>
2023-08-15 21:32:14 +0700

    feat(account):move and update account controller

--- src/controllers/account.controller.mjs
+++ src/controllers/account.controller.mjs
@@ -1,5 +1,6 @@
 import express from 'express';
 import AccountService from '../service/account.service.mjs';
+import PaymentService from '../service/payment.service.mjs';
 
 const router = express.Router();
 
@@ -11,6 +12,20 @@ router.get('/search', async function(req, res){
     }
 });
 
+router.get('/getbill', async function(req, res) {
+    const svc = new PaymentService();
+    res.json(await svc.addPayments({
+        amount: 550000,
+        fromName: 'from',
+        fromAccountNo: '1112223333',
+        fromBank: 'BCA',
+        notes: 'note',
+
+    }, {
+        id: 1123,
+        
+    }))
+});
 
 
 export default router;
