d77551e12ec7cb5f6b2876b456f780357796dc55
nicholassinggih <nicholas.irsyad@akarinti.tech>
2023-08-19 16:27:34 +0700

    feat(bill):add service and controller
    -with search method to get list of bills for a specific account

---
+++ src/controllers/bill.controller.mjs
@@ -0,0 +1,20 @@
+import express from 'express';
+import BillService from '../service/bill.service.mjs';
+
+const router = express.Router();
+
+router.get('/search', async function(req, res){
+    const svc = new BillService();
+    const { accountId, paid, offset, limit } = req.query;
+    let criteria = { accountId, paid };
+    if (paid === undefined) {
+        delete criteria.paid;
+    }
+    const order = [
+        ['billDate', 'DESC']
+    ];
+    const bills = await svc.search(criteria, order, req.query.offset, req.query.limit);
+    res.json( bills );
+});
+
+export default router;
