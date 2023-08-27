4487a87470d9ddd2f07f4fce8e541e709f6cc0d1
nicholassinggih <nicholas.irsyad@akarinti.tech>
2023-08-27 18:36:56 +0700

    refactor(bills):move bill generator to service

--- src/service/bill.service.mjs
+++ src/service/bill.service.mjs
@@ -9,6 +9,33 @@ class BillService {
       billDate: new Date()
     }
   }
+
+  generateBills() {
+    const billSvc = new BillService();
+    const accSvc = new AccountService();
+    const newBills = [];
+    accSvc.getAllActive().then(async accounts => {
+        const updatedAccounts = [];
+        accounts.forEach(acc => {
+            const bill = billSvc.createBillForAccount(acc)
+            newBills.push(bill);
+            if (acc.balance >= bill.amount) {
+                acc.balance -= bill.amount;
+                bill.paid = true;
+                updatedAccounts.push(acc);
+            }
+        });
+        await ConnectionPool.connection.sequelize.transaction(async (trx) => {
+            await Models.Account.bulkCreate(updatedAccounts, {transaction: trx, updateOnDuplicate: ['balance']})
+            await Models.Bill.bulkCreate(newBills, {transaction: trx});
+        });
+
+    }).catch(err => {
+        console.log(err);
+        // TODO: do something here for the error
+    });
+    
+  }
   
   saveBills(billList) {
     Models.Bill.bulkCreate(billList).then(() => {
--- src/service/scheduler.service.mjs
+++ src/service/scheduler.service.mjs
@@ -1,41 +1,13 @@
 import { CronJob } from 'cron';
-import * as Models from '../models/definitions.mjs';
-import ConnectionPool from '../service/connection-pool.mjs';
-import AccountService from './account.service.mjs';
 import BillService from './bill.service.mjs';
 
 class SchedulerService {
     startBillGenerator() {
+        const billService = new BillService();
         new CronJob(
             '0 0 1 * *',    // every 1st of the month
             // '*/10 * * * * *',    // every 10 secs
-            function() {
-                const billSvc = new BillService();
-                const accSvc = new AccountService();
-                const newBills = [];
-                accSvc.getAllActive().then(async accounts => {
-                    const updatedAccounts = [];
-                    accounts.forEach(acc => {
-                        const bill = billSvc.createBillForAccount(acc)
-                        newBills.push(bill);
-                        if (acc.balance >= bill.amount) {
-                            acc.balance -= bill.amount;
-                            bill.paid = true;
-                            updatedAccounts.push(acc);
-                        }
-                    });
-                    await ConnectionPool.connection.sequelize.transaction(async (trx) => {
-                        await Models.Account.bulkCreate(updatedAccounts, {transaction: trx, updateOnDuplicate: ['balance']})
-                        await Models.Bill.bulkCreate(newBills, {transaction: trx});
-                    });
-
-                }).catch(err => {
-                    console.log(err);
-                    // TODO: do something here for the error
-                });
-                
-
-            },
+            billService.generateBills,
             null,
             true,
             'Asia/Jakarta'
